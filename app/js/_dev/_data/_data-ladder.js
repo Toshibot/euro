a8a4b4508a07319ac59f1a1faaec901077229d91
Toshi Stewart <>
2021-06-09 18:17:02 +0200

    cleaned up ladder code

--- app/js/_dev/_data/_data-ladder.js
+++ app/js/_dev/_data/_data-ladder.js
@@ -27,29 +27,22 @@ function dataLadder(data_teams) {
     self.tasksURI = "https://api.football-data.org/v2/competitions/2018/standings";
 
     self.ajax(self.tasksURI, 'GET').done(function(data) {
-        // console.log(data);
 
-        var ladder_a = data.standings[0].table;
-        var ladder_b = data.standings[1].table;
-        var ladder_c = data.standings[2].table;
-        var ladder_d = data.standings[3].table;
-        var ladder_e = data.standings[4].table;
-        var ladder_f = data.standings[5].table;
+        var groups = ['a', 'b', 'c', 'd', 'e', 'f'];
 
+        for (i = 0; i < groups.length; i++){
+            const group = groups[i];
+            const table = data.standings[i].table;
+
+            ladder_build(table, group);
+        }
+        
         // Construct the Ladder
-        for (i = 0; i < ladder_a.length; i++) {
-            const ladder_item_a = ladder_a[i];
-            const ladder_item_b = ladder_b[i];
-            const ladder_item_c = ladder_c[i];
-            const ladder_item_d = ladder_d[i];
-            const ladder_item_e = ladder_e[i];
-            const ladder_item_f = ladder_f[i];
-            ladderItem(ladder_item_a, i+1, data_teams, 'a');
-            ladderItem(ladder_item_b, i+1, data_teams, 'b');
-            ladderItem(ladder_item_c, i+1, data_teams, 'c');
-            ladderItem(ladder_item_d, i+1, data_teams, 'd');
-            ladderItem(ladder_item_e, i+1, data_teams, 'e');
-            ladderItem(ladder_item_f, i+1, data_teams, 'f');
+        function ladder_build(table, group){
+            ladderItem(table[0], 1, data_teams, group);
+            ladderItem(table[1], 2, data_teams, group);
+            ladderItem(table[2], 3, data_teams, group);
+            ladderItem(table[3], 4, data_teams, group);
         }
     })
 
